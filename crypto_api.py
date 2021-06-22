f97ec8f3d88445d664a7f1be9496ba215fd67e85
synphonics <44014224+synphonics@users.noreply.github.com>
2021-06-22 15:30:15 -0400

    Updates

--- crypto_api.py
+++ crypto_api.py
@@ -81,11 +81,15 @@ def turn_to_csv(ticker, df):
 
 alpha_key = os.environ.get('ALPHAVANTAGE_KEY')
 
-def get_ticker_data_av(ticker):
-    url = f"https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_WEEKLY&symbol={ticker}&market=USD&apikey={alpha_key}"
+def get_ticker_data_avd(ticker):
+    url = f"https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol={ticker}&market=USD&apikey={alpha_key}"
     r = requests.get(url)
     data = r.json()
-    df = pd.DataFrame(data)
+    try:
+        df = pd.DataFrame(data)
+    except Exception:
+        print("Invalid crypto ticker")
+        return None
     df = df.iloc[7:]
     df = df.drop(['Meta Data'], axis = 1)
     df.reset_index()
@@ -95,12 +99,61 @@ def get_ticker_data_av(ticker):
     prices_list = []
     time_list = []
     for i in range(len(df.index)):
-        prices_list.append(df['Time Series (Digital Currency Weekly)'][i]['4b. close (USD)'])
+        prices_list.append(df['Time Series (Digital Currency Daily)'][i]['4b. close (USD)'])
         time_list.append(df.index[i])
     df_new = df_new.assign(price = prices_list)
     df_new = df_new.assign(time = time_list)
     df_new = df_new.rename(columns = {'price': price, 'time': time})
     return df_new
+
+def date_split(date):
+    #input is in the form of '2020-04-27'
+    date = date.split('-')
+    next_date = dt.date(int(date[0]), int(date[1]), int(date[2]))
+    return next_date
+
+def date_string(date):
+    #input is a datetime object
+    return date.strftime("%Y-%m-%d")
+
+def get_start_date(ticker, df):
+    #gets the first monday
+    bool = True
+    i = 0
+    initial = df[ticker+' Date'][len(df)-1].split("-")
+    if dt.datetime.today().year - int(initial[0]) < 2 or dt.datetime.today().month > int(initial[1]):
+        bool = False
+        print("2 years of data not available for this ticker")
+        return None
+    while bool:
+        start_date = df[ticker+' Date'][i]
+        date_list = start_date.split('-')
+        if dt.date(int(date_list[0]), int(date_list[1]), int(date_list[2])).weekday() == 0:
+            bool = False
+            print(start_date)
+            return start_date
+        else:
+            i += 1
+
+
+def get_weekly_data(df, start_date, ticker):
+    df_week = pd.DataFrame()
+    test_list = []
+    next_date = start_date
+    test_list.append(next_date)
+    while date_split(df[ticker+' Date'][len(df)-1]) <= date_split(next_date):
+        next_date = date_split(next_date) - dt.timedelta(days = 7)
+        next_date = date_string(next_date)
+        test_list.append(next_date)
+
+    test_list.pop(-1)
+    for i in range(len(test_list)):
+        df_week = df_week.append(df.loc[df[ticker+' Date'] == test_list[i]])
+    price = ticker+' Closing price'
+    time = ticker+' Date'
+    df_week = df_week.rename(columns = {'Close': price, 'Date': time})
+    df_week = df_week.reset_index(drop = True)
+    return df_week
 
 
 def get_crypto_inputs():
@@ -114,10 +167,18 @@ def get_crypto_inputs():
 
 
 def get_crypto_data(ticker_list):
+    if ticker_list == []:
+        return 
     df_final = pd.DataFrame()
     for i in range(len(ticker_list)):
-        df = get_ticker_data_av(ticker_list[i])
-        df_final = pd.concat([df_final, df], axis = 1)
+        df = get_ticker_data_avd(ticker_list[i])
+        sd = get_start_date(ticker_list[i], df)
+        if df.empty or sd is None:
+            print("Something went wrong")
+            break
+        else:
+            df_week = get_weekly_data(df, sd, ticker_list[i])
+        df_final = pd.concat([df_final, df_week], axis = 1)
         if i != 0:
             #print(df_final[ticker_list[i]+' Date'].tolist() == df_final[ticker_list[0]+' Date'].tolist())
             if df_final[ticker_list[i]+' Date'].tolist() == df_final[ticker_list[0]+' Date'].tolist():
@@ -129,13 +190,9 @@ def get_crypto_data(ticker_list):
     df_final = df_final[cols]
     df_final = df_final.rename(columns={ticker_list[0]+' Date': 'Date'})
     df_final = df_final.iloc[:104]
+    df_final = df_final.loc[::-1].reset_index(drop = True) #reverses all the rows
+    df_final = df_final.reset_index(drop = True)
     return df_final
-
 
 def c_turn_to_csv(df):
     df.to_csv('crypto.csv', index = False)
-
-#test implementation
-test = get_crypto_inputs()
-df = get_crypto_data(test)
-c_turn_to_csv(df)
--- stock_api.py
+++ stock_api.py
@@ -1,4 +1,5 @@
 #pip install yfinance
+from stonks.crypto_api import turn_to_csv
 import yfinance as yf
 import pandas as pd
 import datetime as dt
@@ -6,9 +7,12 @@ import datetime as dt
 def get_ticker():
     return input()
 
-def get_ticker_data(ticker):
+def get_stock_ticker_data(ticker):
     the_ticker = yf.Ticker(ticker)
-    hist = the_ticker.history(period="2y")
+    hist = the_ticker.history(period="2y", interval='1wk')
+    if hist.empty:
+        print("won't work chief")
+        return None
     hist = hist.reset_index()
     #type(hist) #pandas dataframe
     df_Stock = pd.DataFrame()
@@ -26,10 +30,15 @@ def date_string(date):
     #input is a datetime object
     return date.strftime("%Y-%m-%d")
 
-def get_start_date(df):
+def get_stock_start_date(df):
     #gets the first monday
     bool = True
     i = 0
+    initial = date_string(df['Date'][i]).split('-')
+    if dt.date.today().year - int(initial[0]) < 2 or dt.date.today().month < int(initial[1]):
+        bool = False
+        print("2 years of data not available for this ticker")
+        return None
     while bool:
         start_date = date_string(df['Date'][i])
         date_list = start_date.split('-')
@@ -40,9 +49,10 @@ def get_start_date(df):
         else:
             i += 1
 
-def get_weekly_data(df, start_date, ticker):
+def get_stock_weekly_data(df, start_date, ticker):
     df_week = pd.DataFrame()
     test_list = []
+    date_list = []
     next_date = start_date
     test_list.append(next_date)
     while df['Date'][len(df)-1] >= date_split(next_date):
@@ -56,6 +66,12 @@ def get_weekly_data(df, start_date, ticker):
     price = ticker+' Closing price'
     time = ticker+' Date'
     df_week = df_week.rename(columns = {'Close': price, 'Date': time})
+    for i in range(len(df_week[time])):
+       date_list.append(date_string(pd.to_datetime(df_week[time].values[i])))
+    df_week = df_week.reset_index(drop = True)
+    df_week[time].update(date_list)
+    if len(df_week) != 104:
+        df_week = df_week.iloc[len(df_week)-104:]
     return df_week
 
 def get_stock_inputs():
@@ -68,12 +84,18 @@ def get_stock_inputs():
     return ticker_list
 
 def get_stock_data(ticker_list):
+    if ticker_list == []:
+        return 
     df_final = pd.DataFrame()
     for i in range(len(ticker_list)):
         try:
-            data = get_ticker_data(ticker_list[i])
-            date1 = get_start_date(data)
-            df = get_weekly_data(data, date1, ticker_list[i])
+            data = get_stock_ticker_data(ticker_list[i])
+            date1 = get_stock_start_date(data)
+            if data.empty or date1 is None:
+                print("Something went wrong")
+                break
+            else:
+                df = get_stock_weekly_data(data, date1, ticker_list[i])
         except (KeyError, IndexError) as e:
             continue
         df_final = pd.concat([df_final, df], axis = 1)
@@ -87,23 +109,56 @@ def get_stock_data(ticker_list):
     cols.append(index)
     df_final = df_final[cols]
     df_final = df_final.rename(columns={ticker_list[0]+' Date': 'Date'})
+    df_final = df_final.reset_index(drop = True)
     return df_final
 
 
 def s_turn_to_csv(df):
     df.to_csv('stock.csv', index = False)
 
-#test implementation
-test = get_stock_inputs()
-df = get_stock_data(test)
-s_turn_to_csv(df)
+# #test implementation
+# test1 = get_stock_inputs()
+# df1 = get_stock_data(test1)
+# s_turn_to_csv(df1)
 
-#random_ticker implementation
-datapath = f"D://work/stonks/random_tickers.csv"
-data = pd.read_csv(datapath)
-the_list = data.values.tolist()
-list_ = []
-for i in range(len(the_list)):
-    list_.append(the_list[i][0])
-tickers_df = get_stock_data(list_)
-s_turn_to_csv(tickers_df)
+# #random_ticker implementation
+# datapath = f"D://work/stonks/random_tickers.csv"
+# data = pd.read_csv(datapath)
+# the_list = data.values.tolist()
+# list_ = []
+# for i in range(len(the_list)):
+#     list_.append(the_list[i][0])
+# tickers_df = get_stock_data(list_)
+# s_turn_to_csv(tickers_df)
+
+
+# #test code
+# ticker = 'AMC'
+# the_ticker = yf.Ticker(ticker)
+# hist = the_ticker.history(period="2y", interval='1wk')
+
+
+# turn_to_csv('qq', hist)
+# hist = hist.reset_index()
+
+# data = yf.download("AMC",interval = '1wk', period='2y')
+
+# d = get_stock_ticker_data('GLBL')
+
+# s = get_stock_start_date(d)
+
+# g = get_stock_weekly_data(d, s, 'AMC')
+
+# type(g['AMD Date'])
+
+# s_turn_to_csv(g)
+
+# len(df1)
+
+# type(df1['Date'].values[0])
+
+
+# df1.reset_index(drop = True, inplace = True)
+
+
+# d.loc[d['Date'] == '2019-09-09']
